8f3ec12fb733a78d0930b58facfd14515bb2f139
colin <colin091508@gmail.com>
2024-09-26 14:32:34 +0800

    feat: add  getBlockInfoByNumber and getBlockHeaderByNumber

--- bin/go_compile.sh
+++ bin/go_compile.sh
@@ -24,7 +24,7 @@ else
     export GOBIN=$GOPATH/bin
     export PATH=$PATH:$GOPATH/bin
 
-    protoc -I ./ --go_out=./ --go-grpc_out=./ savour_rpc/*.proto
+    protoc -I ./ --go_out=./ --go-grpc_out=require_unimplemented_servers=false:. savour_rpc/*.proto
 
     exit_if $?
     echo Done
