93f34d6949eaecd93f756458d74e1aa7eabee824
leo.shi <554601845@qq.com>
2024-11-22 14:28:39 +0800

    feature:  update account.proto (#11)

--- bin/go_compile.sh
+++ bin/go_compile.sh
@@ -1,31 +1,46 @@
 #!/bin/bash
 
-function exit_if() {
-    extcode=$1
-    msg=$2
-    if [ $extcode -ne 0 ]
-    then
-        if [ "msg$msg" != "msg" ]; then
-            echo $msg >&2
-        fi
-        exit $extcode
-    fi
-}
+# 检查是否安装了 Go
+if ! command -v go &> /dev/null; then
+    echo "Error: Go is not installed" >&2
+    exit 1
+fi
 
-echo $GOPATH;
+# 确保 GOPATH 已设置
+if [ -z "$GOPATH" ]; then
+    export GOPATH=$HOME/go
+    echo "GOPATH was not set, using default: $GOPATH"
+fi
 
-if [ ! -f $GOPATH/bin/protoc-gen-go ]
-then
-    echo 'No plugin for golang installed, skip the go installation' >&2
-    echo 'try go get github.com/golang/protobuf/protoc-gen-go' >&2
+# 创建必要的目录
+mkdir -p $GOPATH/bin
+
+# 安装必要的 protoc 插件
+echo "Installing required Go plugins..."
+go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
+if [ $? -ne 0 ]; then
+    echo "Failed to install protoc-gen-go" >&2
+    exit 1
+fi
+
+go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
+if [ $? -ne 0 ]; then
+    echo "Failed to install protoc-gen-go-grpc" >&2
+    exit 1
+fi
+
+# 添加 GOBIN 到 PATH
+export PATH=$PATH:$GOPATH/bin
+
+echo "Compiling protobuf files..."
+protoc -I ./ \
+    --go_out=./ \
+    --go-grpc_out=require_unimplemented_servers=false:. \
+    dapplink/*.proto
+
+if [ $? -eq 0 ]; then
+    echo "Compilation completed successfully"
 else
-    echo Compiling go interfaces...
-    export GO_PATH=$GOPATH
-    export GOBIN=$GOPATH/bin
-    export PATH=$PATH:$GOPATH/bin
-
-    protoc -I ./ --go_out=./ --go-grpc_out=require_unimplemented_servers=false:. dapplink/*.proto
-
-    exit_if $?
-    echo Done
+    echo "Compilation failed" >&2
+    exit 1
 fi
